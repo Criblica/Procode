c051d1baa5487e7d87a2c844a84c6496a49c13e2
Criblica <Criblica@Einar>
2015-07-21 13:10:12 +0200

    Made python image into a link and added python.html Added folder for tutorial html files ;)

--- routes.py
+++ routes.py
@@ -2,17 +2,31 @@ from flask import Flask, render_template
   
 app = Flask(__name__)
   
+
+"""
+    Routes for main menu
+"""
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/tutorials')
 def tutorials():
-    return render_template('tutorials.html')
+    return render_template('tutorials/index.html')
   
 @app.route('/about')
 def about():
     return render_template('about.html')
   
+  
+  
+"""
+    Routes for tutorials
+"""
+
+@app.route('/tutorials/python')
+def python():
+    return render_template('tutorials/python.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
