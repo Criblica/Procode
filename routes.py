fa5b29820b2693710d986d1d189168bf45d84527
Criblica <Criblica@Einar>
2015-07-29 10:33:21 +0200

    Created database and models for user, article, comment Created empty add_tutorial page and link button

--- routes.py
+++ routes.py
@@ -1,13 +1,81 @@
 from flask import Flask, render_template, session, request, redirect, url_for
-from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+#from datetime import timedelta, datetime
 import json
+
    
 app = Flask(__name__)
+
+
+"""
+    Database
+"""
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///development.db"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(16), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False, unique=False)
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    language = db.Column(db.String(255), nullable=False, unique=False)
+    code = db.Column(db.Text, nullable=False, unique=False)
+    explanation = db.Column(db.Text, nullable=False, unique=False)
+    author =  db.Column(db.String(16), nullable=False, unique=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow, unique=False)
+    
+    def __init__(self, language, code, explanation, author, date):
+        self.language = language
+        self.code = code
+        self.explanation = explanation
+        self.author = author
+        self.date = date
+    
+    def to_json(self):
+        return dict(language=self.language, code=self.code, explanation=self.explanation, author=self.author, date=self.date)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.Text, nullable=False, unique=False)
+    author =  db.Column(db.String(16), nullable=False, unique=False) #use user id as primary key instead and do a lookup in users to get username
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow, unique=False)
+    
+    def __init__(self, message, author, date):
+        self.message = message
+        self.author = author
+        self.date = date
+        
+    def to_json(self):
+        return dict(message=self.message, author=self.author, date=self.date)
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False, unique=False)
+    message = db.Column(db.Text, nullable=False, unique=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow, unique=False)
+    
+    def __init__(self, title, message, date):
+        self.title = title
+        self.message = message
+        self.date = date
+        
+    def to_json(self):
+        return dict(title=self.title, message=self.message, date=self.date)
+
+db.create_all()
+
 
 @app.before_request
 def make_session_permanent():
     session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=1) 
+    app.permanent_session_lifetime = datetime.timedelta(hours=12) 
 
 
 """
@@ -30,14 +98,19 @@ def login():
     return render_template('login.html')
 
 @app.route('/validate_login', methods=['GET', 'POST'])
-def validate_login():
-    session['username'] = request.form['username']
-    print session['username']
+def validate_login():   
+    user = User.query.filter_by(username=request.form['username']).first()
+    if user:
+        if user.password == request.form['password']:
+            session['logged_in'] = True
+            session['username'] = user.username
+            return json.dumps({'message':"OK", 'iserror': False})
+        else:
+            return json.dumps({'message': "Wrong password. Please try again!", 'iserror': True})
+    else:
+        return json.dumps({'message': "This username does not exist!", 'iserror': True})
     
-    session['logged_in'] = True
     
-    errorDict = {'message':"OK", 'iserror': False}
-    return json.dumps(errorDict)
 
 @app.route('/logout')
 def logout():
@@ -51,12 +124,22 @@ def registration():
 
 @app.route('/validate_registration', methods=['GET', 'POST'])
 def validate_registration():
-    response = {'message': "OK", 'iserror': False}
-    return json.dumps(response)
+    user = User.query.filter_by(username=request.form['username']).first()
+    if user:
+        message = "This username is taken. Please try another one!"
+        return json.dumps({'message': message, 'iserror': True})
+    
+    db.session.add(User(request.form['username'], request.form['password']))
+    db.session.commit()
+
+    return json.dumps({'message': "OK", 'iserror': False})
   
 """
     Routes for tutorials
 """
+@app.route('/tutorials/add')
+def add_tutorial():
+    return render_template('tutorials/add.html')
 
 @app.route('/tutorials/python')
 def python():
