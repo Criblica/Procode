7cdd7ad5b517fc4e7a0f758015c25b4ab928086e
Criblica <Criblica@Einar>
2015-08-22 19:30:11 +0200

    Cleaning up login and register buttons

--- development.py
+++ development.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     #app.secret_key = 'This is my supersecret key that nobody knows about, except my butcher!!!'
     #app.run()
     app.config.from_object(configs.DevelopmentConfig)
-    app.run(host="192.168.10.168", port=80, debug=True)
+    app.run(host="192.168.10.168", port=5000, debug=True)
     #192.168.10.168
